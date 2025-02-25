758d223097c6f34e27b7b12c7490ad67e64ad3e4
PV SONALI <pv.sonali.official@gmail.com>
2025-02-25 09:18:03 +0530

    added files

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./schema');
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.use(express.json()); 
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+})
+  .then(() => {
+    console.log("Connected to MongoDB database");
+  })
+  .catch(err => {
+    console.error("Error connecting to database:", err);
+  });
+
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = new User(userData);
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res
+        .status(400)
+        .json({ message: 'Validation error', details: error.message });
+    } else {
+      res.status(500).json({ message: 'Server error', details: error.message });
+    }
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
